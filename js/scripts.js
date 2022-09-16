89045b6c9e273bc6f21c170351033723dca5db48
aoc312 <99876171+aoc312@users.noreply.github.com>
2022-09-16 17:14:00 +0100

    updates to live version

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,28 @@ function menuToggle() {
     x.className = 'navtoggle'
   }
 }
+<<<<<<< Updated upstream
+=======
+
+function contactToggle() {
+  var x = document.getElementById('mycontacttoggle')
+  if (x.className === 'contacttoggle') {
+    x.className += ' responsive'
+  } else {
+    x.className = 'contacttoggle'
+  }
+}
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
+>>>>>>> Stashed changes
