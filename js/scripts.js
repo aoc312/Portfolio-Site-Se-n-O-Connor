cd8229460171c3783c73f1d9b30d1815920348eb
aoc312 <99876171+aoc312@users.noreply.github.com>
2022-10-04 17:43:24 +0100

    update image sliders

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 
-
 function menuToggle() {
   var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
@@ -29,4 +28,35 @@ window.onclick = function(e) {
       myDropdown.classList.remove('show');
     }
   }
+}
+
+let slideIndex = [1,1,1];
+let slideId = ["mySlides1", "mySlides2", "mySlides3"]
+let dotId = ["myDots1", "myDots2", "myDots3"]
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function currentSlide(n, no) {
+  showSlides(slideIndex[no] = n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  let dots = document.getElementsByClassName(dotId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  x[slideIndex[no]-1].style.display = "block";
+  dots[slideIndex[no]-1].className += " active";
 }
