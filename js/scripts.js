16b09f3c28d6bfc65cb03d00ebad8a0ea0c155a7
aoc312 <99876171+aoc312@users.noreply.github.com>
2022-10-29 15:09:41 +0100

    update expanding text sections with focus update coming soon cards

--- js/scripts.js
+++ js/scripts.js
@@ -8,73 +8,56 @@ function menuToggle() {
   }
 }
 
-var coll = document.getElementsByClassName('collapsible');
-var i;
+var coll = document.getElementsByClassName('collapsible')
+var i
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener('click', function() {
-    this.classList.toggle('open-section')
-    var content = this.nextElementSibling
-    var icon = document.getElementById('myButtonicon')
-    if (icon.className.baseVal === 'open-icon') {
-      icon.className.baseVal += ' closed'
-    } else {
-      icon.className.baseVal = 'open-icon'
-    }
-    if (content.style.maxHeight) {
-      content.style.maxHeight = null
-    } else {
-      content.style.maxHeight = content.scrollHeight + 'px'
-    }
-  })
+  coll[i].addEventListener('click', buttonToggle)
 }
 
 function buttonToggle() {
-  var toggle = document.getElementById('myProjtoggle')
-  var icon = document.getElementById('myButtonicon')
-  if (toggle.className === 'project-toggle') {
-    toggle.className += ' p-toggled'
+  this.classList.toggle('open-section')
+  var content = this.nextElementSibling
+  var icon = this.querySelector('.svg-section')
+  icon.classList.toggle('closed')
+  if (content.style.maxHeight) {
+    content.style.maxHeight = null
   } else {
-    toggle.className = 'project-toggle'
+    content.style.maxHeight = content.scrollHeight + 'px'
   }
-  if (icon.className.baseVal === 'open-icon') {
-    icon.className.baseVal += ' closed'
-  } else {
-    icon.className.baseVal = 'open-icon'
+  if (this.classList.contains('open-section')) {
+    this.scrollIntoView({block: 'start'})
   }
 }
 
 function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-window.onclick = function(myFunction) {
-  if (!e.target.matches('.dropbtn')) {
-  var myDropdown = document.getElementById("myDropdown");
-    if (myDropdown.classList.contains('show')) {
-      myDropdown.classList.remove('show');
-    }
+  var myDropdown = document.getElementById('myDropdown')
+  if (myDropdown.classList.contains('show')) {
+    myDropdown.classList.remove('show')
+  } else {
+    myDropdown.classList += (' show')
   }
 }
 
-let slideIndex = [1,1,1,1];
-let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4"]
-let dotId = ["myDots1", "myDots2", "myDots3", "myDots4"]
-showSlides(1, 0);
-showSlides(1, 1);
-showSlides(1, 2);
-showSlides(1, 3);
+
+let slideIndex = [1, 1, 1, 1]
+let slideId = ['mySlides1', 'mySlides2', 'mySlides3', 'mySlides4']
+let dotId = ['myDots1', 'myDots2', 'myDots3', 'myDots4']
+showSlides(1, 0)
+showSlides(1, 1)
+showSlides(1, 2)
+showSlides(1, 3)
 
 function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
+  showSlides(slideIndex[no] += n, no)
 }
 
 function currentSlide(n, no) {
-  showSlides(slideIndex[no] = n, no);
+  showSlides(slideIndex[no] = n, no)
 }
 
 function showSlides(n, no) {
-  let i;
+  let i
   let x = document.getElementsByClassName(slideId[no]);
   let dots = document.getElementsByClassName(dotId[no]);
   if (n > x.length) {slideIndex[no] = 1}
