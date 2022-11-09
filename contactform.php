77a8605a1355dcaeaf5411c2649201690cd42ebf
aoc312 <99876171+aoc312@users.noreply.github.com>
2022-11-09 15:54:08 +0000

    update contact form

--- contactform.php
+++ contactform.php
@@ -4,39 +4,12 @@
   $message = $_POST['yourMessage'];
 
   $email_from = 'oconnorsean312@yahoo.ie';
-  $email_subject = "New Form submission";
-  $email_body = "You have received a new message from the user $name.\n". "Here is the message:\n $message".
+  $email_subject = "Website Correspondence";
+  $email_body = "You have received a new message from the user ".$name.":\n\n".$message;
 
-  $to = "oconnorsean312@yahoo.ie";
+  $to = "seanux@seanaoconnor.com";
   $headers = "From: $email_from \r\n";
   $headers .= "Reply-To: $visitor_email \r\n";
+
   mail($to,$email_subject,$email_body,$headers);
-
-  function IsInjected($str) {
-    $injections = array('(\n+)',
-           '(\r+)',
-           '(\t+)',
-           '(%0A+)',
-           '(%0D+)',
-           '(%08+)',
-           '(%09+)'
-           );
-
-    $inject = join('|', $injections);
-    $inject = "/$inject/i";
-
-    if(preg_match($inject,$str))
-    {
-      return true;
-    }
-    else
-    {
-      return false;
-    }
-  }
-
-  if(IsInjected($visitor_email))
-  {
-      echo "Bad email value!";
-      exit;
-  }
+  header("Location: contact_me.php?mailsend");
