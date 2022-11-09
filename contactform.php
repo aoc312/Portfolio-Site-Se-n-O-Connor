43ec75b63f848da526b9016aa7793fd7f22749a2
aoc312 <99876171+aoc312@users.noreply.github.com>
2022-11-09 16:19:50 +0000

    update page extensions and htaccess

--- contactform.php
+++ contactform.php
@@ -3,11 +3,9 @@ if (isset($_POST['submit'])) {
   $name = $_POST['yourName'];
   $visitor_email = $_POST['yourEmail'];
   $message = $_POST['yourMessage'];
-
-  $email_from = 'oconnorsean312@yahoo.ie';
   $email_subject = "Website Correspondence";
-  $email_body = "You have received a new message from ".$name.":\n\n".$message;
 
+  $email_body = "You have received a new message from ".$name.":\n\n".$message;
   $to = "seanux@seanaoconnor.com";
   $headers = "From: ".$visitor_email;
 
