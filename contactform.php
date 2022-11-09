cfed07698402993bf6a695c3dc836ebc5001a77e
aoc312 <99876171+aoc312@users.noreply.github.com>
2022-11-09 16:03:26 +0000

    update

--- contactform.php
+++ contactform.php
@@ -1,17 +1,17 @@
 <?php
-  if (isset($_POST['submit'])) {
-    $name = $_POST['yourName'];
-    $visitor_email = $_POST['yourEmail'];
-    $message = $_POST['yourMessage'];
+if (isset($_POST['submit'])) {
+  $name = $_POST['yourName'];
+  $visitor_email = $_POST['yourEmail'];
+  $message = $_POST['yourMessage'];
 
-    $email_from = 'oconnorsean312@yahoo.ie';
-    $email_subject = "Website Correspondence";
-    $email_body = "You have received a new message from the user ".$name.":\n\n".$message;
+  $email_from = 'oconnorsean312@yahoo.ie';
+  $email_subject = "Website Correspondence";
+  $email_body = "You have received a new message from the user ".$name.":\n\n".$message;
 
-    $to = "seanux@seanaoconnor.com";
-    $headers = "From: ".$email_from;
-    $headers .= "Reply-To: ".$visitor_email;
+  $to = "seanux@seanaoconnor.com";
+  $headers = "From: ".$email_from;
+  $headers .= "Reply-To: ".$visitor_email;
 
-    mail($to, $email_subject, $email_body, $headers);
-    header("Location: contact_me.php?mailsend");
-  }
+  mail($to, $email_subject, $email_body, $headers);
+  header("Location: contact_me.php?mailsend");
+}
